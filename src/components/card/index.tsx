beb8668f38b9a8fee8d4622741341f8611316f0e
Zak Nesler <znesler@itswoop.com>
2023-01-12 10:34:27 -0500

    add preview

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { State } from '../../constants/states'
 import type { Carrier } from '../../types/carrier'
 import { CarrierSelector } from '../selectors/carrier'
@@ -5,20 +6,18 @@ import { StateSelector } from '../selectors/state'
 import styles from './styles.module.css'
 
 export const Card = () => {
-  const onCarrierChange = (carrier: Carrier | null) => {
-    console.log(carrier?.name)
-  }
-
-  const onStateChange = (state: State | null) => {
-    console.log(state?.code)
-  }
+  const [carrier, setCarrier] = useState<Carrier | null>(null)
+  const [state, setState] = useState<State | null>(null)
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.dashed}>
         <div className={styles.selectors}>
-          <CarrierSelector onChange={onCarrierChange} />
-          <StateSelector onChange={onStateChange} />
+          <div>
+            {JSON.stringify({ carrier: carrier?.name, state: state?.name })}
+          </div>
+          <CarrierSelector onChange={setCarrier} />
+          <StateSelector onChange={setState} />
         </div>
       </div>
     </div>
