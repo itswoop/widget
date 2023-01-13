c6154996c798f5be139859c4cd96f12280925dcf
Zak Nesler <znesler@itswoop.com>
2023-01-13 11:21:33 -0500

    add craco for postcss config

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+/** @type {import('@craco/types').CracoConfig} */
+module.exports = {
+  style: {
+    postcss: {
+      loaderOptions: postcssLoaderOptions => {
+        postcssLoaderOptions.postcssOptions.plugins = [
+          require('postcss-custom-media')({
+            importFrom: './src/css/variables/dimensions.css',
+          }),
+          require('postcss-nested'),
+          ...postcssLoaderOptions.postcssOptions.plugins,
+        ]
+
+        return postcssLoaderOptions
+      },
+    },
+  },
+}
