85ba40ba13068def8a8437b9289582c0a20020aa
Zak Nesler <znesler@itswoop.com>
2023-01-30 15:51:42 -0500

    add legalese

--- src/components/premium-calculator/components/table/index.tsx
+++ src/components/premium-calculator/components/table/index.tsx
@@ -27,49 +27,57 @@ export const PremiumTable: React.FC<PremiumTableProps> = ({
   const gridTemplateColumns = `repeat(${years.length + 1}, 1fr)`
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.tableWrapper}>
-        <table className={styles.table}>
-          <thead>
-            <tr style={{ gridTemplateColumns }}>
-              <th className={styles.fixed} />
-              {years.map(year => (
-                <th key={year}>{year}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            <tr style={{ gridTemplateColumns }}>
-              <th className={styles.fixed}>% Change</th>
-              {yearsWithChange.map(({ year, percentInteger }) => (
-                <td key={year}>
-                  {percentInteger === null
-                    ? '–'
-                    : `${formatDecimalString(percentInteger, 1)}%`}
-                </td>
-              ))}
-            </tr>
-
-            <tr className={styles.summary} style={{ gridTemplateColumns }}>
-              <th className={styles.fixed}>Annual payment</th>
-              {compoundValues.map((value, index) => (
-                <td key={index}>{formatMoneyString(value)}</td>
-              ))}
-            </tr>
-          </tbody>
-        </table>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.tableWrapper}>
+          <table className={styles.table}>
+            <thead>
+              <tr style={{ gridTemplateColumns }}>
+                <th className={styles.fixed} />
+                {years.map(year => (
+                  <th key={year}>{year}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              <tr style={{ gridTemplateColumns }}>
+                <th className={styles.fixed}>% Change</th>
+                {yearsWithChange.map(({ year, percentInteger }) => (
+                  <td key={year}>
+                    {percentInteger === null
+                      ? '–'
+                      : `${formatDecimalString(percentInteger, 1)}%`}
+                  </td>
+                ))}
+              </tr>
+              <tr className={styles.summary} style={{ gridTemplateColumns }}>
+                <th className={styles.fixed}>Annual payment</th>
+                {compoundValues.map((value, index) => (
+                  <td key={index}>{formatMoneyString(value)}</td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <p className={styles.explainer}>
+          {carrier} has {percentChange >= 0 ? 'increased' : 'decreased'} their
+          premiums by {formatDecimalString(percentChange, 1)}% in {state} over
+          the last {years.length - 1} years. This means that if your annual
+          insurance payment was{' '}
+          <strong>{formatMoneyString(startingPremium)}</strong> in {years[0]},
+          you would be paying{' '}
+          <strong>
+            {formatMoneyString(compoundValues[compoundValues.length - 1])}
+          </strong>{' '}
+          today.
+        </p>
       </div>
 
-      <p className={styles.explainer}>
-        {carrier} has {percentChange >= 0 ? 'increased' : 'decreased'} their
-        premiums by {formatDecimalString(percentChange, 1)}% in {state} over the
-        last {years.length - 1} years. This means that if your annual insurance
-        payment was <strong>{formatMoneyString(startingPremium)}</strong> in{' '}
-        {years[0]}, you would be paying{' '}
-        <strong>
-          {formatMoneyString(compoundValues[compoundValues.length - 1])}
-        </strong>{' '}
-        today.
+      <p className={styles.legal}>
+        This information is aggregate data from public insurance filings and is
+        only intended for illustrative purposes. Significant variation may occur
+        based on individual experience or incomplete data. Please connect your
+        policy for specific details based on your individual experience.
       </p>
     </div>
   )
